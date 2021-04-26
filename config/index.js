4b99e0df4a9dc434973d1e9649f938e859dbec03
anshaysaboo <anshaysaboo1586@gmail.com>
2021-04-25 19:55:40 -0700

    Added Resume

--- config/index.js
+++ config/index.js
@@ -84,7 +84,7 @@ module.exports = {
     button: {
       useFileName: true,
       name: "Resume",
-      fileName: "goldenpuppy.pdf", // the file has to be placed inside the static folder at the root level
+      fileName: "Resume.pdf", // the file has to be placed inside the static folder at the root level
       url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
     },
   },
